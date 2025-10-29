b919120d89e36825c4b3b8108b861230f40a1bd5
Akshatha16092001 <akshatha.poojari@indivillage.co>
2025-10-29 10:03:23 +0530

    now adding the focalpoint

--- Assets/Scenes/PlayerController.cs
+++ Assets/Scenes/PlayerController.cs
@@ -4,13 +4,13 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;        // How fast the player moves
     private Rigidbody playerRb;       // Reference to the player's Rigidbody
-    //private GameObject focalPoint;    // Reference to the focal point for camera direction
+    private GameObject focalPoint;    // Reference to the focal point for camera direction
     private float forwardInput;       // Input from player (W/S or Up/Down keys)
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-       // focalPoint = GameObject.Find("Focal Point");
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     void Update()
@@ -19,6 +19,6 @@ public class PlayerController : MonoBehaviour
         forwardInput = Input.GetAxis("Vertical");
 
         // Move the player in the direction the camera (focal point) is facing
-        playerRb.AddForce(Vector3.forward * speed * forwardInput);
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
 }
